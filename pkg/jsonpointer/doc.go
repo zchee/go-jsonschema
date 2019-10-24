3ffd96a6bc997ea131e33f5a6e05772be1c6aa95
Koichi Shiraishi <zchee.io@gmail.com>
2019-10-24 22:16:19 +0900

    pkg/jsonpointer: add initial jsonpointer package

---
+++ pkg/jsonpointer/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The go-jsonschema Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package jsonpointer an implementation of JSON Pointer for Go.
+package jsonpointer
