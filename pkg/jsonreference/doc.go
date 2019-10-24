8e250b237e03e5adab7e8b4f57654df07438f047
Koichi Shiraishi <zchee.io@gmail.com>
2019-10-24 22:16:36 +0900

    pkg/jsonreference: add initial jsonreference package

---
+++ pkg/jsonreference/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The go-jsonschema Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package jsonreference an implementation of JSON Reference for Go.
+package jsonreference
